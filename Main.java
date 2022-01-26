d527d345540c50695535ed0074d1912692ad223c
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2022-01-25 18:28:28 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,19 +4,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.print("---3D Tic Tac Toe Menu---");
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Will player X be AI or Human? ");
-        String pX = keyboard.nextLine();
-        System.out.print("Will player O be AI or Human? ");
-        String pO = keyboard.nextLine();
-        if(pO.equals("AI") && pX.equals("AI"))
-        {
-            System.out.print("How many milliseconds do you want the AI to wait between moves? ");
-            int waitTime = keyboard.nextInt();
-                    
-        }
         TFrame f = new TFrame("3D Tic Tac Toe");
-
+        Thread t = new Thread(f.getP());
+        t.start();
     }
 }
