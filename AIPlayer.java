e84f91b893d7eabe487b3c2a789bf78545b8f21b
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2022-02-01 21:55:46 -0600

    Add files via upload

---
+++ AIPlayer.java
@@ -0,0 +1,32 @@
+public class AIPlayer implements PlayerInt
+{
+    private char letter;
+    private Location move;
+    private String name;
+
+    public AIPlayer(char letter, String name)
+    {
+        this.letter = letter;
+        this.name = name;
+    }
+
+    @Override
+    public char getLetter() {
+        return letter;
+    }
+
+    @Override
+    public Location getMove(char[][][] board) {
+        return move;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
