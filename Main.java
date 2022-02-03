5c5dadc6fb083c714c596567e872b9d04cb3d4dd
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2022-02-03 10:53:35 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,5 @@ public class Main
     public static void main(String[] args)
     {
         TFrame f = new TFrame("3D Tic Tac Toe");
-        Thread t = new Thread(f.getP());
-        t.start();
     }
 }
