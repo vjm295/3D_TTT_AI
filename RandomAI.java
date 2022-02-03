31854912af649669e3d90dd0eb804ba33d975343
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2022-02-03 10:54:28 -0600

    Add files via upload

---
+++ RandomAI.java
@@ -0,0 +1,36 @@
+public class RandomAI extends Player implements PlayerInt
+{
+    private char letter;
+    private String name;
+
+    public RandomAI(char letter, String name)
+    {
+        super(letter, name);
+        this.letter = letter;
+        this.name = name;
+    }
+
+    @Override
+    public char getLetter() {
+        return letter;
+    }
+
+    @Override
+    public Location getMove(char[][][] board) {
+        Location move;
+        do {
+            move = new Location((int) (Math.random() * 4), (int) (Math.random() * 4), (int) (Math.random() * 4));
+        } while(board[move.getSheet()][move.getRow()][move.getCol()] == 'x' || board[move.getSheet()][move.getRow()][move.getCol()] == 'o');
+        return move;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
